931e3aaf9b0dea77b9e2f2b73fa800334d514713
Tim Vanloocke <tim.vanloocke@roeselare.be>
2020-06-04 10:00:33 +0200

    error handling register form

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -7,14 +7,17 @@ import { rgbToHex } from "../helpers/helpers";
 
 export default class RegisterForm extends Component {
   state = {
+    errors: [],
     displayColorPicker: false,
     color: {
       r: "241",
       g: "112",
       b: "19",
-      a: "1"
-    }
+      a: "1",
+    },
   };
+
+  errorStyle = { color: "red", textAlign: "left", paddingTop: "10px" };
 
   handleClick = () => {
     this.setState({ displayColorPicker: !this.state.displayColorPicker });
@@ -24,28 +27,33 @@ export default class RegisterForm extends Component {
     this.setState({ displayColorPicker: false });
   };
 
-  handleChange = color => {
+  handleChange = (color) => {
     this.setState({ color: color.rgb });
   };
 
-  register = values => {
+  register = (values) => {
     API.post("api/users", {
       first_name: values.first_name,
       last_name: values.last_name,
       email: values.email,
       password: values.password,
       favorite_color: rgbToHex(this.state.color),
-      avatar: "https://api.adorable.io/avatars/285/" + values.email
-    }).then(response => {
-      alert(response.statusText);
-    });
+      avatar: "https://api.adorable.io/avatars/285/" + values.email,
+    }).then(
+      (res) => {
+        // alert(res.statusText);
+      },
+      (error) => {
+        let errors = error.response.data;
+        this.setState({ errors });
+      }
+    );
   };
 
   // Formik
 
-  submitHandler = values => {
+  submitHandler = (values) => {
     this.register(values);
-    console.log(values);
   };
 
   render() {
@@ -55,7 +63,7 @@ export default class RegisterForm extends Component {
           width: "36px",
           height: "14px",
           borderRadius: "2px",
-          background: `rgba(${this.state.color.r}, ${this.state.color.g}, ${this.state.color.b}, ${this.state.color.a})`
+          background: `rgba(${this.state.color.r}, ${this.state.color.g}, ${this.state.color.b}, ${this.state.color.a})`,
         },
         swatch: {
           padding: "5px",
@@ -63,20 +71,20 @@ export default class RegisterForm extends Component {
           borderRadius: "1px",
           boxShadow: "0 0 0 1px rgba(0,0,0,.1)",
           display: "inline-block",
-          cursor: "pointer"
+          cursor: "pointer",
         },
         popover: {
           position: "absolute",
-          zIndex: "2"
+          zIndex: "2",
         },
         cover: {
           position: "fixed",
           top: "0px",
           right: "0px",
           bottom: "0px",
-          left: "0px"
-        }
-      }
+          left: "0px",
+        },
+      },
     });
 
     return (
@@ -89,7 +97,7 @@ export default class RegisterForm extends Component {
           email: "",
           password: "",
           favorite_color: "#800000",
-          avatar: "https://api.adorable.io/avatars/285/"
+          avatar: "https://api.adorable.io/avatars/285/",
         }}
       >
         <Form>
@@ -103,6 +111,10 @@ export default class RegisterForm extends Component {
                   name="first_name"
                   placeholder="Voornaam"
                 ></Field>
+                <div style={this.errorStyle} className="error">
+                  {this.state.errors.first_name &&
+                    this.state.errors.first_name[0]}
+                </div>
               </div>
               <div className="col">
                 <Field
@@ -111,38 +123,54 @@ export default class RegisterForm extends Component {
                   name="last_name"
                   placeholder="Naam"
                 ></Field>
+                <div style={this.errorStyle} className="error">
+                  {this.state.errors.last_name &&
+                    this.state.errors.last_name[0]}
+                </div>
               </div>
             </div>
-            <Field
-              type="email"
-              name="email"
-              className="form-control mb-4"
-              placeholder="E-mail"
-            ></Field>
-            <Field
-              type="password"
-              name="password"
-              className="form-control"
-              placeholder="Paswoord"
-            ></Field>
-            <label htmlFor="favorite_color">Kies je kleur: </label>
-            {/* <Field type="text" name="favorite_color"></Field> */}
-            <div
-              className="form-control"
-              style={styles.swatch}
-              onClick={this.handleClick}
-            >
-              <div style={styles.color} />
-            </div>{" "}
-            {this.state.displayColorPicker ? (
-              <div style={styles.popover}>
-                <div style={styles.cover} onClick={this.handleClose} />
-                <BlockPicker
-                  color={this.state.color}
-                  onChange={this.handleChange}
-                />
+            <div className="form-row mb-4">
+              <Field
+                type="email"
+                name="email"
+                className="form-control"
+                placeholder="E-mail"
+              ></Field>
+              <div style={this.errorStyle} className="error">
+                {this.state.errors.email && this.state.errors.email[0]}
               </div>
-            ) : null}
+            </div>
+            <div className="form-row">
+              <Field
+                type="password"
+                name="password"
+                className="form-control"
+                placeholder="Paswoord"
+              ></Field>
+            </div>
+            <div style={this.errorStyle} className="error">
+              {this.state.errors.password && this.state.errors.password[0]}
+            </div>
+            <div className="form-row">
+              <label htmlFor="favorite_color">Kies je kleur: </label>
+              {/* <Field type="text" name="favorite_color"></Field> */}
+              <span
+                className="form-control"
+                style={styles.swatch}
+                onClick={this.handleClick}
+              >
+                <div style={styles.color} />
+              </span>{" "}
+              {this.state.displayColorPicker ? (
+                <div style={styles.popover}>
+                  <div style={styles.cover} onClick={this.handleClose} />
+                  <BlockPicker
+                    color={this.state.color}
+                    onChange={this.handleChange}
+                  />
+                </div>
+              ) : null}
+            </div>
             {/* <label htmlFor="avatar">Url Avatar: </label>
             <Field className="form-control" type="text" name="avatar"></Field> */}
             <button className="btn btn-info my-4 btn-block" type="submit">
